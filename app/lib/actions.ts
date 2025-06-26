32b5673a772f7192ba6e2538cfa3ad71fd7cfebb
RohaidAhmed <rohaidahmed123@gmail.com>
2025-06-26 18:35:48 +0500

    fourth commit

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -1,6 +1,8 @@
 'use server';
 
+import { signIn } from '@/auth';
 import { error, log } from 'console';
+import { AuthError } from 'next-auth';
 import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
 import postgres from 'postgres';
@@ -55,7 +57,7 @@ export async function createInvoice(prevState: State, formData: FormData) {
     const { customerId, amount, status } = validatedFields.data;
     const amountInCents = amount * 100;
     const date = new Date().toISOString().split('T')[0];
-    
+
     try {
         await sql`
         INSERT INTO invoices (customer_id, amount, status, date)
@@ -106,4 +108,24 @@ export async function deleteInvoice(id: string) {
     }
     revalidatePath('/dashboard/invoices');
 
+}
+
+
+export async function authenticate(
+    prevState: string | undefined,
+    formData: FormData,
+) {
+    try{
+        await signIn('credentials', formData);
+    }catch(error){
+        if (error instanceof AuthError) {
+            switch (error.type){
+                case 'CredentialsSignin':
+                    return 'Invalid credentials.';
+                default:
+                    return 'something went wrong.';
+            }
+        }
+        throw error;
+    }
 }
